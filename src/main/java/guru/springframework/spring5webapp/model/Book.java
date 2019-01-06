e19aff71881eab9e7cd280f9e2aab750e541423f
Ythalo Rossy <ythalorossy@gmail.com>
2019-01-05 21:07:22 -0500

    Organizes the annotation @ManyToMany

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -17,8 +17,10 @@ public class Book {
     private String publisher;
 
     @ManyToMany
-    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
-    inverseJoinColumns = @JoinColumn(name = "author_id"))
+    @JoinTable(
+    		name = "author_book", 
+    		joinColumns = @JoinColumn(name = "book_id"),
+    		inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
